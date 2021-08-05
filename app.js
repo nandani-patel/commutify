2f0766468a0c01fe8073fb7ee3cd389a157331d1
nandani-patel <nandanip271@gmail.com>
2021-08-05 23:32:19 +0530

    added message func

--- app.js
+++ app.js
@@ -5,17 +5,29 @@ const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+
 const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
-    // console.log("new connection formed");
+
+    socket.on('disconnect', ()=>{
+        io.emit('message',`a user has left the chat`);
+    });
+
     socket.emit('message','welcome to commutify!');
-})
+
+    socket.broadcast.emit('message',`a user has entered the chat`);
+
+    socket.on('chatmsg', (text)=>{
+        console.log(text);
+        io.emit('message',text);
+    });
+});
 
 server.listen(8000, ()=>{
     console.log("server running sucessfully at port 8000");
-})
+});
 
 
--- public/main.js
+++ public/main.js
@@ -2,4 +2,28 @@ const socket = io();
 
 socket.on('message',message => {
     console.log(message);
-})
+    send(message);
+});
+
+const  chat = document.getElementById('text');
+const chatmessage = document.getElementById('right');
+
+chat.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const text = e.target.elements.msg.value;
+    console.log(text);
+    
+    socket.emit('chatmsg', text);
+
+    msg.value="";
+});
+
+function send(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+
+    div.innerHTML=`${message}`;
+
+    chatmessage.appendChild(div);
+};
